6ca83e3df6f8d749ef28b0785f701f4fdd6890af
JAA999 <JArteaga999@icloud.com>
2022-01-17 13:34:52 -0700

    Initial Commit

---
+++ Perform_v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Perform_v1
+//
+//  Created by Joseph Arteaga on 1/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Perform_v1/Perform_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Perform_v1App.swift
+//  Perform_v1
+//
+//  Created by Joseph Arteaga on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Perform_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
